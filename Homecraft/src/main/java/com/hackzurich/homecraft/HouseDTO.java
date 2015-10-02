c77726d7fb29def1dc166e1fbf8d930263cf0672
Marta Rożek <mr360953@students.mimuw.edu.pl>
2015-10-03 01:59:08 +0200

    connecting with HomeGate

---
+++ Homecraft/src/main/java/com/hackzurich/homecraft/HouseDTO.java
@@ -0,0 +1,15 @@
+package com.hackzurich.homecraft;
+
+import java.util.ArrayList;
+
+public class HouseDTO {
+	public double latitude;
+	public double longitude;
+	public int numberRooms;
+	public int floor;
+	public double sellingPrice;
+	public ArrayList<String> pictures;
+	public boolean lift;
+	public boolean balcony;
+
+}
