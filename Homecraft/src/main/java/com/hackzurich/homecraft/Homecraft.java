99524b5bc0338283f65952ec3d3b8fbefeb8086a
Sandro Meier <sandro.meier@fidelisfactory.ch>
2015-10-03 01:58:29 +0200

    Add bukkit server

--- Homecraft/src/main/java/com/hackzurich/homecraft/Homecraft.java
+++ Homecraft/src/main/java/com/hackzurich/homecraft/Homecraft.java
@@ -3,5 +3,16 @@ package com.hackzurich.homecraft;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Homecraft extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		// Insert logic that is run when plugin is started.
+		getLogger().info("onEnable has been invoked");
+	}
+	
+	@Override
+	public void onDisable() {
+		// Logic which is run before shutdown.
+		getLogger().info("onDisable has been invoked");
+	}
 
 }
---
+++ server/README.md
@@ -0,0 +1,11 @@
+## Start server
+
+Run following command to start server: 
+
+    java -Xmx1024M -jar craftbukkit-1.8.8.jar -o true
+
+## Connect
+
+Use in Multiplayer -> Direct Connect
+
+    localhost:25565
