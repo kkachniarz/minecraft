6236c5c25d4defe6404059190fdf0baef7c5b170
Sandro Meier <sandro.meier@fidelisfactory.ch>
2015-10-03 10:56:45 +0200

    First example of generating blocks

--- Homecraft/src/main/java/com/hackzurich/homecraft/HomecraftCommandExecutor.java
+++ Homecraft/src/main/java/com/hackzurich/homecraft/HomecraftCommandExecutor.java
@@ -1,8 +1,12 @@
 package com.hackzurich.homecraft;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class HomecraftCommandExecutor implements CommandExecutor {
 	private final Homecraft plugin;
@@ -14,7 +18,17 @@ public class HomecraftCommandExecutor implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-		sender.sendMessage("Foooooo!");
+		if (!(sender instanceof Player)) {
+			return false;
+		}
+		Player player = (Player)sender;
+		
+		Location playerLocation = player.getLocation();
+		int x = playerLocation.getBlockX();
+		int y = playerLocation.getBlockY();
+		int z = playerLocation.getBlockZ();
+		Block currentBlock = player.getWorld().getBlockAt(x+5, y, z);
+		currentBlock.setType(Material.DIAMOND_BLOCK);
 		
 		return false;
 	}
