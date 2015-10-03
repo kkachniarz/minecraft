f77855cb3cb61693171d4477ea3f329f4f7e9e93
Sandro Meier <sandro.meier@fidelisfactory.ch>
2015-10-03 10:20:44 +0200

    org.json setup

--- Homecraft/src/main/java/com/hackzurich/homecraft/Homecraft.java
+++ Homecraft/src/main/java/com/hackzurich/homecraft/Homecraft.java
@@ -1,14 +1,21 @@
 package com.hackzurich.homecraft;
 
-import java.io.IOException;
-
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Homecraft extends JavaPlugin {
 	@Override
 	public void onEnable() {
+	
 		// Setup the HomecraftCommandExecutor
 		getCommand("homecraft").setExecutor(new HomecraftCommandExecutor(this));
+        
+        // Try connector
+        try {
+            getLogger().info(HomegateManager.getData().toString());
+        } catch (Exception e) {
+        // TODO Auto-generated catch block
+        	e.printStackTrace();
+        }
 	}
 	
 	@Override
@@ -16,6 +23,4 @@ public class Homecraft extends JavaPlugin {
 		// Logic which is run before shutdown.
 		getLogger().info("onDisable has been invoked");
 	}
-	
-	
 }
--- Homecraft/src/main/java/com/hackzurich/homecraft/HomecraftCommandExecutor.java
+++ Homecraft/src/main/java/com/hackzurich/homecraft/HomecraftCommandExecutor.java
@@ -18,5 +18,4 @@ public class HomecraftCommandExecutor implements CommandExecutor {
 		
 		return false;
 	}
-	
 }
